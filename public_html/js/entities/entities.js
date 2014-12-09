e6f2d093ca7fb082e6ccf316382d399862c1dddd
Blair Wright <blairbwright@gmail.com>
2014-12-09 15:06:03 -0800

    new

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -11,7 +11,7 @@ game.PlayerEntity = me.Entity.extend({
                     return (new me.Rect(0, 0, 30, 128)).toPolygon();
                 }
             }]);
-
+        // I added the 80 at the end of the animation for the speed of how fast they would move at a certain speed
         this.renderable.addAnimation("idle", [3]);
         this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80);
 
@@ -20,16 +20,24 @@ game.PlayerEntity = me.Entity.extend({
         this.body.setVelocity(5, 20);
         me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
     },
+    // the if statement was if thee player pressed the right key the character would move right
     update: function(delta) {
         if (me.input.isKeyPressed("right")) {
             this.body.vel.x += this.body.accel.x * me.timer.tick;
-        if (me.input.isKeyPressed("up")) {
-            this.body.vel.x += this.body.accel.x * me.timer.tick;
+
+        } else if (me.input.isKeyPressed("left")) {
+            this.body.vel.x -= this.body.accel.x / me.timer.tick;
 
         } else {
             this.body.vel.x = 0;
         }
-        this.body.update(delta);3
+
+        if (me.input.isKeyPressed("up")) {
+            this.body.vel.y -= this.body.accel.y * me.timer.tick;
+
+        }
+// animations for small walk
+        this.body.update(delta);
         me.collision.check(this, true, this.collideHandler.bind(this), true);
         if (this.body.vel.x !== 0) {
             if (!this.renderable.isCurrentAnimation("smallWalk")) {
@@ -50,7 +58,7 @@ game.PlayerEntity = me.Entity.extend({
     }
 
 });
-
+// level trigger to spawn
 game.LevelTrigger = me.Entity.extend({
     init: function(x, y, settings) {
         this._super(me.Entity, 'init', [x, y, settings]);
